d3bc02a4bd2e2d0982f2ce647295a063763d367e
NeoKokuxz <neko.kokubyakuin@yahoo.com>
2020-05-16 22:27:05 -0400

    project 2 complete upload

--- project2/read.php
+++ project2/read.php
@@ -41,59 +41,68 @@
     <div class="succ">
         <?php
 
-        $user = $_POST['username'];
-        $password = $_POST['password'];
+$user = $_POST['username'];
+$password = $_POST['password'];
 
-        if (!empty($user) ){
+if (!empty($user)) {
 
-            //Heroku database
-            $host = "us-cdbr-east-06.cleardb.net";
-            $dbUsername = "b4b3860ef90b7a";
-            $dbPassword = "117519ad";
-            $dbname = "heroku_4179949f5f78b65";
+    //Heroku database
+    $host = "us-cdbr-east-06.cleardb.net";
+    $dbUsername = "b4b3860ef90b7a";
+    $dbPassword = "117519ad";
+    $dbname = "heroku_4179949f5f78b65";
 
-            //Local database
-            // $host = "localhost";
-            // $dbUsername = "root";
-            // $dbPassword = "sunsh1ne";
-            // $dbname = "cisc4800";
+    //Local database
+    // $host = "localhost";
+    // $dbUsername = "root";
+    // $dbPassword = "sunsh1ne";
+    // $dbname = "cisc4800";
 
-            $conn = mysqli_connect($host, $dbUsername, $dbPassword, $dbname);
-            $query = "SELECT * FROM data1 WHERE user='$user' && password=$password;";
+    $conn = mysqli_connect($host, $dbUsername, $dbPassword, $dbname);
+    $query = "SELECT * FROM data1 WHERE user='$user' && password=$password;";
 
-            $result = mysqli_query($conn, $query);
+    $result = mysqli_query($conn, $query);
 
-            if($result){
-                $num = mysqli_num_rows($result);
-            } else {
-                $num = 0 ;
+    if ($result) {
+        $num = mysqli_num_rows($result);
+    } else {
+        $num = 0;
+    }
+
+    if ($num > 0) {
+        echo "Sucessful log in! </br> </br>";
+        //Change here!
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "name: " . $row["user"] . "</br>";
+                echo "Welcome Back! </br></br>";
             }
 
-            if($num > 0){ 
-                echo "Sucessful log in! </br> </br>";
-                //Change here!
-                $query = "SELECT * FROM data2;" ;
-                if (mysqli_num_rows($result) > 0) {
-                    // output data of each row
-                    while($row = mysqli_fetch_assoc($result)) {
-                    echo "name: " . $row["user"]. " - password: " . $row["password"];
-                    }
-                } else {
-                    echo "0 results";
+            $query1 = "SELECT * FROM data2 ORDER BY number DESC;";
+            $result = mysqli_query($conn, $query1);
+            if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                echo "Current Stock data </br>";
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "Stock: " . $row["message"] . " Price: " . $row["number"];
+                    echo " </br>";
                 }
-
-            }else {
-                echo "failed!";
+            } else {
+                echo "0 results";
             }
 
-            mysqli_close($conn);
+        } else {
+            echo "failed!";
+        }
 
-
-            } else {
-                echo "All field are required!";
-                die();
-        }?>
-    </div>
+        mysqli_close($conn);
+    } else {
+        echo "All field are required!";
+        die();
+    }
+}?>
+</div>
 </body>
 
 </html>
